7e93e7a2a8c638fa7dbf87d0c00d6971559c617e
Luke Aiken <lukeaiken@gmail.com>
2013-08-05 14:16:27 -0600

    Rock_Paper_Scissors
    * current passing tests for Luke_Player
    
    [#54641022]

---
+++ spec/lib/luke_player_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe LukePlayer do
+  it 'should be a Lukeplayer' do
+    expect(subject).to be_a(LukePlayer)
+  end
+
+  describe "play" do
+    my_plays = []
+    let(:gambits) {subject.load_gambits}
+    it "plays things in order" do
+      gambits.each do |gambit|
+        my_plays << gambit
+        expect(my_plays).to include(gambit)
+      end
+    end
+  end
+
+  describe '.won' do
+
+    it "increases 'win_count' by 1" do
+      expect{subject.won}.to change(subject, :win_count).from(0).to(1)
+    end
+
+    it "increases 'win_count' by 1 on second win" do
+      expect{2.times {subject.won}}.to change(subject, :win_count).from(0).to(2)
+    end
+  end
+
+
+  describe '.lost' do
+
+    it "increases 'lost_count' by 1" do
+      expect{subject.lost}.to change(subject, :lost_count).from(0).to(1)
+    end
+
+    it "increases 'lost_count' by 1 on second loss" do
+      expect{2.times {subject.lost}}.to change(subject, :lost_count).from(0).to(2)
+    end
+  end
+
+  describe '.tied' do
+
+    it "increases 'tied_count' by 1" do
+      expect{subject.tied}.to change(subject, :tied_count).from(0).to(1)
+    end
+
+    it "increases 'tied_count' by 1 on second tie" do
+      expect{2.times {subject.tied}}.to change(subject, :tied_count).from(0).to(2)
+    end
+  end
+
+end
